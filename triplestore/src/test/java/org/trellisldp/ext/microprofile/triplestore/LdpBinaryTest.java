7df601e7cb8bd6b433478d129d0ec20d8171fc1c
Aaron Coburn <acoburn@apache.org>
2019-10-04 17:08:21 -0400

    Add triplestore-based application
    Resolves #1

---
+++ triplestore/src/test/java/org/trellisldp/ext/microprofile/triplestore/LdpBinaryTest.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.trellisldp.ext.microprofile.triplestore;
+
+import static java.util.concurrent.TimeUnit.MINUTES;
+import static javax.ws.rs.client.ClientBuilder.newBuilder;
+
+import io.quarkus.test.common.http.TestHTTPResource;
+import io.quarkus.test.junit.QuarkusTest;
+
+import java.net.URL;
+
+import javax.ws.rs.client.Client;
+
+import org.trellisldp.test.LdpBinaryTests;
+
+@QuarkusTest
+class LdpBinaryTest implements LdpBinaryTests {
+
+    @TestHTTPResource
+    URL baseUrl;
+
+    String resource;
+
+    Client client = newBuilder().connectTimeout(2, MINUTES).build();
+
+    @Override
+    public void setResourceLocation(final String location) {
+        resource = location;
+    }
+
+    @Override
+    public String getResourceLocation() {
+        return resource;
+    }
+
+    @Override
+    public String getBaseURL() {
+        return baseUrl.toString();
+    }
+
+    @Override
+    public Client getClient() {
+        return client;
+    }
+}
+
